4e42e9529e48bd17a934af187a56a23f781f1895
AdriAG1 <adrimaarru@gmail.com>
2023-10-26 21:03:41 +0200

    6

--- src/ej2_05.py
+++ src/ej2_05.py
@@ -3,6 +3,7 @@ Ejercicio 2.1.5
 
 Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.
 '''
+
 def pedirNumero():
     '''
     Pide un numero por consola
@@ -12,7 +13,6 @@ def pedirNumero():
     num = int(input())
 
     return num
-
 
 
 def main():
@@ -28,8 +28,6 @@ def main():
     
 
     
-
-   
 
 if __name__ == "__main__":
     main()
